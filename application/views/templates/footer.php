372b2f602a7d39f432a2099feddc6a2af76ea9bb
Auriana <auriana.hug@heig-vd.ch>
2014-11-06 09:31:40 +0100

    udpating header and footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,6 @@
-<strong>&copy; 2011</strong>
-</body>
+
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="<?php echo asset_url().'js/bootstrap.min.js'; ?>"></script>		
+	</body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,20 @@
 <html>
-<head>
-	<title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
-</head>
-<body>
-	<h1>CodeIgniter 2 Tutorial</h1>
+	<head>
+		<title><?php echo $title ?></title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Craz'event - Gestionnaire d'événements">
+		<meta name="author" content="">
+
+		<!--
+		Favicon
+		<link rel="icon" href="../../favicon.ico">
+		-->
+
+		<link rel="stylesheet" type="text/css" href="<?php echo asset_url().'css/bootstrap.min.css'; ?>">
+		<link rel="stylesheet" type="text/css" href="<?php echo asset_url().'css/bootstrap-theme.min.css'; ?>">
+		<link rel="stylesheet" type="text/css" href="<?php echo asset_url().'css/ce-style.css'; ?>">
+	</head>
+	<body role="document">
+		<h1>Craz'event</h1>
