acbe2901502647b339053f584a6dd1c0e84cf113
Auriana <auriana.hug@heig-vd.ch>
2014-11-19 14:28:19 +0100

    adding details_event

--- application/controllers/details_event.php
+++ application/controllers/details_event.php
@@ -6,15 +6,17 @@ class Details_Event extends CI_Controller {
          parent::__construct();
     }
 
-    function index()
+    function index($id)
     {
         if($this->session->userdata('logged_in')) //TODO : moyen sûr de check login ?
         {
-            $data['title'] = 'Détails de l\'événement';
-
+			$data['title'] = 'Détails de l\évènement';
+			
+			$info_event = $this -> details($id);
+			
             $this->load->helper(array('form'));
             $this->load->view('templates/header', $data);
-            $this->load->view('pages/details_event_view');
+            $this->load->view('pages/details_event_view', $info_event);
             $this->load->view('templates/footer');
         }
         //if user is not logged in : redirection to login
@@ -22,6 +24,32 @@ class Details_Event extends CI_Controller {
         {  
             redirect('home', 'refresh');
         }
+    }
+	
+	function details($id) {
+      	$this->load->model('event','',TRUE);
+		
+		$info_event = array();
+
+        $result = $this->event->get_event($id);
+		foreach($result as $row) {
+			$info_event['name'] = $row -> name;
+			$info_event['private'] = $row -> private;
+			$info_event['invitation_suggestion_allowed'] = $row -> invitation_suggestion_allowed;
+			$info_event['description'] = $row -> description;
+			$info_event['start_date'] = $row -> start_date;
+			$info_event['inscription_deadline'] = $row -> inscription_deadline;
+			$info_event['duration'] = $row -> duration;
+			$info_event['start_place'] = $row -> start_place;
+			$info_event['participant_max_nbr'] = $row -> participant_max_nbr;
+			$info_event['participant_minimum_age'] = $row -> participant_minimum_age;
+			$info_event['organizer'] = $row -> organizer;
+			$info_event['individual_proposition_suggestion_allowed'] = $row -> individual_proposition_suggestion_allowed;
+			$info_event['region'] = $row -> region;
+		}
+		
+		return $info_event;
+		
     }
 
 }
--- application/views/pages/details_event_view.php
+++ application/views/pages/details_event_view.php
@@ -1,6 +1,32 @@
 <div class="col-md-12 white-bloc centred">
 	<h1 class="text-centred">
-		Nom de l'événement
+		<?php echo $name; ?>
 	</h1>
+	<h5>
+		<?php echo ($private = 1 ? "Privé" : "Public" ); ?>
+	</h5>
+	<div id="link-answer">
+		<a href="#" alt=""><?php echo ($private = 1 ? "Répondre à l'invitation" : "S'inscrire" ); ?></a>
+	</div>
+	<p class="bloc-info"> 
+		<?php echo $description; ?>
+	</p>
+	<ul class="bloc-info">
+		<li><?php echo $start_date; ?></li>
+		<li>S'inscrire jusqu'au <?php echo $inscription_deadline; ?></li>
+		<li>A <?php echo $start_place; ?></li>
+	</ul>
+	<ul class="bloc-info">
+		<b>Activités</b>
+		<li>activité 1</li>
+	</ul>
+	<ul class="bloc-info">
+		<b>Checklist</b>
+		<li>truc</li>
+	</ul>
+	<ul class="bloc-info">
+		<li>Nombre maximum de participants: <?php echo $participant_max_nbr; ?></li>	
+		<li>Age minimal requis : <?php echo $participant_minimum_age; ?></li>
+	</ul>
 
 </div>
