5da5c56b93e001ecc00da7851f34ee79f483547a
schodaw <dominique.jollien@heig-vd.ch>
2014-12-11 23:54:17 +0100

    fixing bad git merging

--- application/views/pages/create_user_view.php
+++ application/views/pages/create_user_view.php
@@ -1,4 +1,3 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>	
 <script>
     function checkdate(m, d, y) {
       //  discuss at: http://phpjs.org/functions/checkdate/
