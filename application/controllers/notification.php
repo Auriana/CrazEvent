3fdb43f6dccd8483f25aa0315733bc1135df1a0e
schodaw <dominique.jollien@heig-vd.ch>
2015-01-04 22:15:23 +0100

    fixing footer

--- application/controllers/notification.php
+++ application/controllers/notification.php
@@ -15,15 +15,15 @@ class Notification extends CI_Controller {
         if($this->session->userdata('logged_in')) {
             $data['title'] = 'Notifications';
 			$session_data = $this->session->userdata('logged_in');
-            $data['notifications'] = $this->user->get_messages($session_data['id'], $offset, 5);
+            $data['notifications'] = $this->user->get_messages($session_data['id'], $offset, 10);
 			$data['nb_notifications'] = $this->user->count_unread_message($session_data['id']);
-            $data['nextOffset'] = $offset + 5;
-            $data['previousOffset'] = $offset - 5;
+            $data['nextOffset'] = $offset + 10;
+            $data['previousOffset'] = $offset - 10;
             
             $this->load->helper(array('form'));
             $this->load->view('templates/header_logged_in', $data);
             $this->load->view('pages/notification_view');
-            $this->load->view('templates/sticky-footer');
+            $this->load->view('templates/footer');
         //if user is not logged in : redirection to welcome page
         } else {  
             redirect('welcome', 'refresh');
